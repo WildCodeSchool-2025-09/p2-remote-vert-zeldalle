1bb42ba82156f6bf8ab496785ea10eea93b15065
Matthieu Bergeron <Matt-77130@hotmail.fr>
2025-11-18 18:05:25 +0100

    ajout de la Navigation bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,28 +1,41 @@
-import { useLocation } from "react-router-dom"
-import { Link } from "react-router-dom"
-import recipesIcon from "../assets/icons/IconRecette.png"
-import inventoryIcon from "../assets/icons/IconInventaire.png"
-import ingredientsIcon from "../assets/icons/IconIngrédient.png" 
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import ingredientsIcon from "../assets/icons/IconIngredient.png";
+import inventoryIcon from "../assets/icons/IconInventaire.png";
+import recipesIcon from "../assets/icons/IconRecette.png";
 
 function NavigateBar() {
-    const location = useLocation()
-    const recipesActive = location.pathname === "/recipes"
-    const inventoryActive = location.pathname === "/inventory"
-    const ingredientActive = location.pathname === "/ingredients"
-    return (
-        <div>
-        <Link to = "/recipes" className={recipesActive ? "ButtonRecipes active" : "ButtonRecipes"}>
-        <img src={recipesIcon} alt="" />
-        <span>Recettes</span>
-        </Link>
-        <Link to = "/inventory"className={inventoryActive ? "ButtonInventory active" : "ButtonInventory"}>
-        <img src={inventoryIcon} alt="Inventaire" />
-        <span>Inventaire</span>
-        </Link>
-        <Link to = "/ingredients"className={ingredientActive ? "ButtonIngredients active" : "ButtonIngredients"}>
-        <img src={ingredientsIcon} alt="Ingrédients" />
-        <span>Ingrédients</span>
-        </Link>
-        </div>
-    )
+	const location = useLocation();
+	const recipesActive = location.pathname === "/recipes";
+	const inventoryActive = location.pathname === "/inventory";
+	const ingredientActive = location.pathname === "/ingredients";
+	return (
+		<div>
+			<Link
+				to="/recipes"
+				className={recipesActive ? "ButtonRecipes active" : "ButtonRecipes"}
+			>
+				<img src={recipesIcon} alt="" />
+				<span>Recettes</span>
+			</Link>
+			<Link
+				to="/inventory"
+				className={
+					inventoryActive ? "ButtonInventory active" : "ButtonInventory"
+				}
+			>
+				<img src={inventoryIcon} alt="Inventaire" />
+				<span>Inventaire</span>
+			</Link>
+			<Link
+				to="/ingredients"
+				className={
+					ingredientActive ? "ButtonIngredients active" : "ButtonIngredients"
+				}
+			>
+				<img src={ingredientsIcon} alt="Ingrédients" />
+				<span>Ingrédients</span>
+			</Link>
+		</div>
+	);
 }
