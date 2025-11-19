56cc8b35199bdb513a453ed85e9cded4e445bdc8
Hugo Lobato <hugo.lobato110@gmail.com>
2025-11-19 15:44:33 +0100

    WIP: sauvegarde avant pull

--- src/contexts/InventoryContext.tsx
+++ src/contexts/InventoryContext.tsx
@@ -1,28 +1,23 @@
-import { type ReactNode, createContext, useContext, useState } from "react";
+import { type ReactNode, createContext, useContext, useState } from"react";
 import type { InventoryItemProps } from "../type";
 
-type InventoryItem = {
-	inventory: InventoryItemProps[];
-	setInventory: React.Dispatch<React.SetStateAction<InventoryItem[]>>;
-};
-const InventoryContext = createContext({
-	inventory: [] as InventoryItemProps[],
-	addIngredient: (item: InventoryItemProps) => {},
-});
+type InventoryItem ={
+    inventory: InventoryItemProps[];
+    setInventory: React.Dispatch<React.SetStateAction<InventoryItem[]>>;
+}
+const InventoryContext = createContext({inventory : [] as InventoryItemProps[], addIngredient: (item: InventoryItemProps) =>{},});
 
-export default function InventoryProvider({
-	children,
-}: { children: ReactNode }) {
-	const [inventory, setInventory] = useState<InventoryItemProps[]>([]);
-	const addIngredient = (item: InventoryItemProps) => {};
+export default function InventoryProvider({ children }: { children: ReactNode}) {
+    const [inventory, setInventory] = useState<InventoryItemProps[]>([]);
+    const addIngredient = (item: InventoryItemProps) =>{}
 
-	return (
-		<InventoryContext.Provider value={{ inventory, addIngredient }}>
-			{children}
-		</InventoryContext.Provider>
-	);
+        return (
+            <InventoryContext.Provider value={{ inventory, addIngredient }}>
+                {children}
+            </InventoryContext.Provider>
+        );
 }
 
 export const useInventory = () => {
-	return useContext(InventoryContext);
-};
+    return useContext(InventoryContext);
+}
